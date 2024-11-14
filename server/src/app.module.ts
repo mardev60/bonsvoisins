bd8c81bf0394bbc62bb2c1ca9b1073b1cda0f3a9
mardev60 <makil.uspn@gmail.com>
2024-11-14 19:18:37 +0100

    Suppression files de base

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [ConfigModule.forRoot(), UsersModule],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  controllers: [],
+  providers: [PrismaService],
   exports: [PrismaService],
 })
 export class AppModule {}
