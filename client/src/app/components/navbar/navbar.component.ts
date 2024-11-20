c40c877dd1bacbb59f8b61a018055de8ccf5bd56
mardev60 <makil.uspn@gmail.com>
2024-11-20 19:44:51 +0100

    Maj icons

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { ApiService } from '../../core/services/api.service';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,17 +6,9 @@ import { Router } from '@angular/router';
   templateUrl: './navbar.component.html'
 })
 export class NavbarComponent {
-  navItems = [
-    { link: '/dashboard/home', label: 'Découvrir', iconPath: 'm19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z' },
-    { link: '/dashboard/collecte', label: 'Collecte', iconPath: 'M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z' },
-    { link: '/dashboard/proposer', label: 'Proposer', iconPath: 'M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z' },
-    { link: '/dashboard/profil', label: 'Profil', iconPath: 'M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z' }
-  ];
-
-
   @Output() logout = new EventEmitter<void>();
 
-  constructor(private apiService : ApiService, private router: Router) {}
+  constructor(private router: Router) {}
 
   logoutUser() {
     this.logout.emit();
@@ -25,9 +16,5 @@ export class NavbarComponent {
 
   isActive(link: string): boolean {
     return this.router.url === link;
-  }
-
-  async fetchUser() {
-    const user = await this.apiService.get<any>('').subscribe(user => {console.log(user)});
   }
 }
