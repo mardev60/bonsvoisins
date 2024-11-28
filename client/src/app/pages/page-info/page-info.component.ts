63153a2eb7841d8e4dafca5451d9d56a94bd2fb6
mardev60 <makil.uspn@gmail.com>
2024-11-28 18:07:27 +0100

    Refacto

--- client/src/app/pages/page-info/page-info.component.ts
+++ client/src/app/pages/page-info/page-info.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-page-info',
   templateUrl: './page-info.component.html',
   styleUrl: './page-info.component.scss'
 })
-export class PageInfoComponent {
+export class PageInfoComponent implements OnInit {
+  adress: string = '8 rue Lesiour';
+  city: string = 'Montataire';
+  coverImage: string = '../../../assets/meal2.png';
+  userImage: string = 'https://a.storyblok.com/f/191576/1200x800/a3640fdc4c/profile_picture_maker_before.webp';
+  userName: string = 'Utilisateur';
+  dishName: string = 'Plat'; 
+  timeRange: string = 'Plage horaire';
 
+  showSendMessage: boolean = false;
+  showCodeBox: boolean = true;
+  showCollectorGuide: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 }
