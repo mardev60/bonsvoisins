d5da15df1dbd33f902d00675d569c1003c059b01
devbu <emmanuelniasse@gmail.com>
2024-12-01 14:59:14 +0100

    wip

--- client/src/app/pages/meal-suggestion/meal-suggestion.component.ts
+++ client/src/app/pages/meal-suggestion/meal-suggestion.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MealsService } from '../../core/services/meals.service';
 import { getGroupLabel } from '../../utils/date/get-group-label';
+import { GroupedMeals } from '../../types/db-entities.type';
 
 @Component({
   selector: 'app-meal-suggestion',
@@ -8,15 +9,86 @@ import { getGroupLabel } from '../../utils/date/get-group-label';
   styleUrl: './meal-suggestion.component.scss'
 })
 export class MealSuggestionComponent {
+  mealsToSuggest: any[] = [];
+  mealsToSuggestComplete: any[] = [];
+  groupedMeals: GroupedMeals = {};
+  isLoading = false;
 
   constructor(private mealsService: MealsService) {}
 
   ngOnInit(): void {
-    this.mealsService.fetchMealsByUser();
+    this.fetchMeals();
   }
 
-  getGroupedMeals() {
-    return this.mealsService.getGroupedMeals();
+  private fetchMeals(): void {
+    this.isLoading = true;
+    this.mealsService.fetchMealsByUser().subscribe((res) => {
+      this.isLoading = false;
+      this.mealsToSuggestComplete = res;
+      this.mealsToSuggest = this.mealsToSuggestComplete.map(this.mapMealData.bind(this));
+      this.groupCommandsByDate();
+    }
+  )};
+
+  private mapMealData(meal: any): any {
+    console.log(meal);
+    return {
+      id: meal.id,
+      id_author: meal.id_author,
+      collected_address: meal.collected_address,
+      collect_city: meal.collect_city,
+      collect_code: meal.collect_code,
+      createdat: new Date(meal.createdat),
+      date_end: new Date(meal.date_end),
+      date_start: new Date(meal.date_start),
+      description: meal.description,
+      is_active: meal.is_active,
+      name: meal.name,
+      photo_url: meal.photo_url,
+      updatedat: new Date(meal.updatedat),
+      user: `${meal.user.last_name} ${meal.user.first_name}`,
+      user_avatar: meal.user.avatar,
+    };
+  }
+
+  private groupCommandsByDate(): void {
+    const today = new Date();
+
+    this.groupedMeals = this.mealsToSuggest.reduce(
+      (groups: any, meal) => {
+        console.log(meal);
+        
+        const key = this.getDateGroupKey(meal.createdat, today);
+
+        if (!groups[key]) {
+          groups[key] = [];
+        }
+        groups[key].push(meal);
+        return groups;
+      },
+      {}
+    );
+  }
+
+  private getDateGroupKey(mealDate: Date, today: Date): string {
+    const normalizedCommandDate = new Date(mealDate.getFullYear(), mealDate.getMonth(), mealDate.getDate());
+    const normalizedToday = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+    
+    const daysDifference = Math.floor(
+      (normalizedToday.getTime() - normalizedCommandDate.getTime()) / (1000 * 3600 * 24)
+    );
+  
+    if (daysDifference === 0) {
+      return "Aujourd'hui";
+    } else if (daysDifference === 1) {
+      return 'Hier';
+    } else {
+      return `Il y a ${daysDifference} jours`;
+    }
+  }  
+  
+  getGroupedMealsKeys(): string[] {
+    return Object.keys(this.groupedMeals);
   }
 
   getGroupLabel(key: string): string {
